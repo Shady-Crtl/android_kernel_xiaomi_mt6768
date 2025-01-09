06182998821fa7ecec8d8c68674a54e22eb051a3
Shady Mohamed Ali <sh4dymo74med@gmail.com>
2025-01-09 13:02:11 +0200

    Update user.h

--- include/linux/sched/user.h
+++ include/linux/sched/user.h
@@ -42,7 +42,7 @@ struct user_struct {
 	atomic_long_t locked_vm;
 #endif
 
-#ifdef CONFIG_KSU_SUSFS
+  #ifdef CONFIG_KSU_SUSFS
       u64 android_kabi_reserved2;
   #endif
       // Android KABI reserved fields
